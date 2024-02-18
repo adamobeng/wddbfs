2c85b6de7a16feb8fa7eea47c16b6645862a250f
Adam Obeng <git@adamobeng.com>
2024-02-18 15:37:22 -0800

    Add a test case for #1

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,8 @@
+import sqlite3
+import pandas as pd
+import pandas.testing
+import io
+
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
 
@@ -31,3 +36,33 @@ class TestDBResourceProvider(TestCase):
 
         table_artifact = dbcollection.get_member("test_table.csv")
         self.assertEqual(table_artifact.get_content(), table_contents)
+
+# TODO: clean this up by handling mocks in setUp
+expected = pd.DataFrame({"path": ["/test"], "content": [b"\x01\x02\x03"], "size": [99]})
+ConnectionMock = MagicMock(spec=sqlite3.Connection)
+ConnectionMock.execute.return_value.description = [[c] for c in expected.columns]
+ConnectionMock.execute.return_value.fetchall.return_value = [expected.iloc[0].tolist()]
+
+
+class TestDBResourceProviderFromSqlite(TestCase):
+    @patch("wddbfs.main.DB.con", new=ConnectionMock)
+    @patch(
+        "wddbfs.main.DB.table_names", new=["test_table"]
+    )  # So that we don't have to also return the table names in the sqlite connection mock
+    def test_table_contents_with_blobs(self):
+        rp = wddbfs.main.DBResourceProvider(db_paths=["test.sqlite"])
+        root = rp.get_resource_inst("/", MagicMock())
+        self.assertEqual(root.get_member_names(), ("test.sqlite",))
+
+        dbcollection = root.get_member("test.sqlite")
+        self.assertEqual(dbcollection.db.table_names, ["test_table"])
+
+        table_artifact = dbcollection.get_member("test_table.jsonl")
+        result = pd.read_json(io.BytesIO(table_artifact.get_content().read()), lines=True)
+        pandas.testing.assert_frame_equal( result, expected)
+
+        table_artifact = dbcollection.get_member("test_table.csv")
+        result = pd.read_csv(io.BytesIO(table_artifact.get_content().read()))
+        pandas.testing.assert_frame_equal( result, expected)
+
+
